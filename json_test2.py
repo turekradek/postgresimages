996dccd3cfd4525b05df92114aff40769e90c947
turekradek <turekradek@gmail.com>
2023-10-22 15:31:35 +0200

    parse

--- json_test2.py
+++ json_test2.py
@@ -3,11 +3,52 @@ import uuid
 import random
 from datetime import datetime, timedelta
 import os 
+import sys
+import argparse 
 
+parser = argparse.ArgumentParser(description='take arguments ')
+# Add command-line arguments
+parser.add_argument('file_to_read', help='The file to read.')
+parser.add_argument('file_name', nargs='?', help='The name of the output file (optional).')
+# Parse the command-line arguments
+args = parser.parse_args()
+# Access the values
+file_to_read = args.file_to_read
+file_name = args.file_name
+print(f'file_to_read = {file_to_read}')
+print(f'file_name = {file_name}')
+# args = parser.parse_args()
+# print( f'args parser : %s' % args)
 # Create a list to store the data
 # data = []
 columns = []
-directory = r'Wyniki.txt'
+# directory = r'Wyniki.txt'
+# file_name = r'data3.json'
+# file_to_read = None
+# file_name = None
+dicto = { }
+for i in range(len(sys.argv)):
+    print( f' i = {i} arg {sys.argv[i]}')
+    dicto[i] = sys.argv[i]
+print( dicto )
+if n := len(sys.argv) == 2:
+    print('if n == 2')
+    file_to_read = sys.argv[1]
+    print( f' file_to_read = {file_to_read}')
+elif n == 3:
+    file_to_read = sys.argv[1]
+    file_name = sys.argv[2]
+    print('if n == 3')
+    print( f' file_to_read = {file_to_read}')
+    print( f' file_name = {file_name}')
+else:
+    # file_to_read = r'Wyniki.txt'
+    # file_name = r'data3.json'
+    print( len(sys.argv) , 'to jest else ')
+    
+print( f'sys.argv =  {sys.argv}')    
+print( f' file_to_read = {file_to_read}')
+print( f' file_name = {file_name}')
 
 def read_columns_from_file(file_name, separator ):
     
@@ -18,7 +59,7 @@ def read_columns_from_file(file_name, separator ):
     print(f'2 columns {columns}')
     return columns 
 
-columns = read_columns_from_file( directory, ';')
+columns = read_columns_from_file( file_to_read, ';')
 print( ' these are columns from txt file {}'.format(columns) )
 
 def create_dictionary(  columns: list ):
@@ -40,26 +81,27 @@ def create_data( file_name: str, dictionary: dict ):
     for index, linia in enumerate(content):
         resutl = {}
         linia = linia.split(';')
-        print( f'linia : {linia}')
-        resutl[keys[0]] = linia[0]
-        # print( f'=== 0 resutl {resutl }')
-        resutl[keys[1]] = linia[1]
-        # print( f'=== 1 resutl {resutl }')
-        resutl[keys[2]] = linia[2]
-        # print( f'=== 2 resutl {resutl }')
-        #resutl[keys[3]] = linia[3]
-        # print( f'=== 3 resutl {resutl }')
+        # print( f'linia : {linia}')
+        for i in range(len(keys)):
+            resutl[keys[i]] = linia[i]
+        # # print( f'=== 0 resutl {resutl }')
+        # resutl[keys[1]] = linia[1]
+        # # print( f'=== 1 resutl {resutl }')
+        # resutl[keys[2]] = linia[2]
+        # # print( f'=== 2 resutl {resutl }')
+        # #resutl[keys[3]] = linia[3]
+        # # print( f'=== 3 resutl {resutl }')
         data.append( resutl ) 
         # print( f' index {index} \n data _+_+_+_+_+ {data}' )
         result = {}
-    print( data, f'\n\n {len(data)}' )
+    print( data[:10], f'\n\n {len(data)}' )
     return  data 
 
 
-data1 =  create_data(directory, dictionary )
+data1 =  create_data(file_to_read, dictionary )
 
 # Write the data to a JSON file
-with open('data3.json', 'w') as f:
+with open(file_name , 'w') as f:
     json.dump(data1, f)
     
 # print( f' data1 {data1}\n\n\n]')
