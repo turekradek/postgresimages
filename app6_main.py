28887d36af2726e3db7de9ef0ec8cf697c03109d
rturek1 <radoslaw.turek@dxc.com>
2023-10-26 11:01:37 +0200

    added excel sheets to df to sql

--- app6_main.py
+++ app6_main.py
@@ -6,6 +6,7 @@ import os
 import sys 
 import pandas as pd
 import numpy as np
+from typing import Dict, List, Tuple, Union, Any, Optional
 import requests
 import zipfile
 import xlsxwriter
@@ -55,7 +56,21 @@ files_json = 'data3.json'
     
 ############ json test all .py 
 def read_columns_from_file(file_name, separator ):
-    
+    """
+    Reads the first line of a text file, which is assumed to contain column names separated by the specified separator,
+    and returns the column names as a list.
+
+    Parameters:
+    - file_name (str): The name of the file to read from.
+    - separator (str): The separator used to split the column names.
+
+    Returns:
+    - list of str: A list of column names extracted from the file's first line.
+
+    Example:
+    If the file's first line is "Name, Age, Address" and the separator is ",", the function will return
+    ['Name', 'Age', 'Address'] as the list of column names.
+    """
     file = open( file_name , 'r' )
     columns = file.readline().strip()
     print(f'1 columns {columns}')
@@ -64,12 +79,46 @@ def read_columns_from_file(file_name, separator ):
     return columns 
 
 def create_dictionary(  columns: list ):
+    """
+    Creates a dictionary where each column name is a key, and its corresponding value is set to the same name.
+
+    Parameters:
+    - columns (list): A list of column names to be used as keys in the dictionary.
+
+    Returns:
+    - dict: A dictionary where each column name is a key, and its value is set to the same name.
+
+    Example:
+    If you call create_dictionary(['Name', 'Age', 'Address']), it will return a dictionary like this:
+    {'Name': 'Name', 'Age': 'Age', 'Address': 'Address'}
+    """
     dictionary = {}
     for name in columns:
         dictionary[name] = name
     return dictionary
 
 def create_data( file_name: str, dictionary: dict ):
+    """
+    Reads data from a text file using a provided dictionary to interpret the columns, and returns a list of dictionaries
+    where each dictionary represents a row of data with column names as keys.
+
+    Parameters:
+    - file_name (str): The name of the file to read data from.
+    - dictionary (dict): A dictionary that maps column names to their positions in each line of the file.
+
+    Returns:
+    - list of dict: A list of dictionaries, where each dictionary represents a row of data with column names as keys.
+
+    Example:
+    If you have a file with the following content:
+    "Name;Age;Address"
+    "John;30;123 Main St"
+    "Alice;25;456 Elm St"
+    
+    And you call create_data('data.txt', {'Name': 0, 'Age': 1, 'Address': 2}), it will return a list like this:
+    [{'Name': 'John', 'Age': '30', 'Address': '123 Main St'},
+     {'Name': 'Alice', 'Age': '25', 'Address': '456 Elm St'}]
+    """
     file = open( file_name ,  'r' )
     content = [  line. strip() for line in  file.readlines() ]
     content = content[1:]    
@@ -88,6 +137,17 @@ def create_data( file_name: str, dictionary: dict ):
     return  data 
 
 def write_data_to_json(file_name, data1):
+    """
+    Writes the given data to a JSON file.
+
+    Parameters:
+    - file_name (str): The name of the JSON file to write the data to.
+    - data1: The data to be written to the JSON file.
+
+    Example:
+    If you call write_data_to_json('output.json', {'Name': 'John', 'Age': 30, 'Address': '123 Main St'}),
+    it will write the data to 'output.json' in JSON format.
+    """
     with open(file_name , 'w') as f:
         json.dump(data1, f)
         
@@ -96,6 +156,23 @@ def write_data_to_json(file_name, data1):
 
 ############ app6 .py 
 def check_json_keys(file_path):
+    """
+    Reads a JSON file and checks if it contains a list of dictionaries. If so, it returns the keys of the first dictionary.
+
+    Parameters:
+    - file_path (str): The path to the JSON file to be checked.
+
+    Returns:
+    - list or None: If the JSON file contains a list of dictionaries, it returns the keys of the first dictionary as a list.
+      If the JSON file does not match the expected structure, it returns None.
+
+    Example:
+    If you have a JSON file 'data.json' with the following content:
+    [{"Name": "John", "Age": 30, "Address": "123 Main St"},
+     {"Name": "Alice", "Age": 25, "Address": "456 Elm St"}]
+     
+    Calling check_json_keys('data.json') will return ['Name', 'Age', 'Address'].
+    """
     with open(file_path) as f:
         data = json.load(f)
     if isinstance(data, list) and len(data) > 0 and isinstance(data[0], dict):
@@ -105,6 +182,24 @@ def check_json_keys(file_path):
         return None
 
 def insert_data_to_table(cur, conn, data, keys_in_json):
+    """
+    Inserts data into a database table using a cursor and a database connection.
+
+    Parameters:
+    - cur: The database cursor for executing SQL queries.
+    - conn: The database connection.
+    - data (list of dict): The data to be inserted, where each dictionary represents a row of data.
+    - keys_in_json (list of str): The keys to extract values from dictionaries in the 'data' list.
+    - table_name (str): The name of the database table to insert data into.
+
+    Example:
+    If you have a list of dictionaries 'data' and a list of keys 'keys_in_json' like this:
+    data = [{'Name': 'John', 'Age': 30, 'Address': '123 Main St'},
+            {'Name': 'Alice', 'Age': 25, 'Address': '456 Elm St'}]
+    keys_in_json = ['Name', 'Age', 'Address']
+
+    You can call insert_data_to_table(cur, conn, data, keys_in_json, 'my_table_name') to insert the data into the specified database table.
+    """
     try:
         placeholders = ', '.join(['%s'] * len(keys_in_json))
         columns = ', '.join(keys_in_json)
@@ -190,32 +285,32 @@ def data_frame_to_sql(data_frame,table_name,  engine):
     
 
 ######################
-data = create_dataframe('data2.json')
-print( data.head(20))
+#data = create_dataframe('data2.json')
+#print( data.head(20))
 #data.to_sql(name='kiersql', con=con)
 # Establish a connection to the PostgreSQL database
-conn = psycopg2.connect(
-    dbname="postgres",
-    user="postgres",
-    password="radek",
-    host="localhost", # or the Docker Machine's IP if you're using Docker Toolbox
-    port="5432"
-)
-print( 'CONNECTION CREATED')
-cur = conn.cursor()
-print( 'CUR DONE')
+#conn = psycopg2.connect(
+#    dbname="postgres",
+#    user="postgres",
+#    password="radek",
+#    host="localhost", # or the Docker Machine's IP if you're using Docker Toolbox
+#    port="5432"
+#)
+#print( 'CONNECTION CREATED')
+#cur = conn.cursor()
+#print( 'CUR DONE')
 # create_database( cur, conn , new_database ) 
 # engine = create_engine(f'postgresql://{postgres_username}:{postgres_password}@localhost:5432/{new_database}')
 
 ########################################## DODANE Z app61.py
 #engine_database = sa.create_engine( f'postgresql://{postgres_username}:{postgres_password}@localhost:5432/' )
-create_database( cur, conn , new_database ) 
+#create_database( cur, conn , new_database ) 
 #engine = create_engine(f'postgresql://{postgres_username}:{postgres_password}@localhost:5432/{new_database}')
-print( 'engine CREATED')
-read_table(cur, conn, 'kierowcy')
-df_unavailable_sessions = create_dataframe_from_excel_sheet('unavailable_sessions_.xlsx', 'Unavailable sessions')
-print( df_unavailable_sessions.head(20))
-df_unavailable_sessions_part = df_unavailable_sessions.iloc[:200]
+#print( 'engine CREATED')
+#read_table(cur, conn, 'kierowcy')
+#df_unavailable_sessions = create_dataframe_from_excel_sheet('unavailable_sessions_.xlsx', 'Unavailable sessions')
+#print( df_unavailable_sessions.head(20))
+#df_unavailable_sessions_part = df_unavailable_sessions.iloc[:200]
 # engine1 = create_engine(f'postgresql://{postgres_username}:{postgres_password}@localhost:5432/exceltest')
 
 #data_frame_to_sql(data_frame,table_name,  engine)
@@ -245,7 +340,7 @@ df_unavailable_sessions_part = df_unavailable_sessions.iloc[:200]
 #table.create(engine)
 #data.to_sql( table_name, engine, if_exists='replace', index=False)
 
-print( 'TABLE CREATED ' )
+#print( 'TABLE CREATED ' )
 # Check if the table exists
 #insp = inspect(engine)
 #if not insp.has_table(f'{table_name}'):
@@ -281,8 +376,39 @@ print( 'TABLE CREATED ' )
 #except Exception as e:
 #    print("Error occurred while selecting data:", e)
 
-conn.commit()
-conn.close()
+#conn.commit()
+#conn.close()
 
-# if __name__=='__main__':
-#     pass
+if __name__=='__main__':
+    conn = psycopg2.connect(
+        dbname="postgres",
+        user="postgres",
+        password="radek",
+        host="localhost", # or the Docker Machine's IP if you're using Docker Toolbox
+        port="5432"
+    )
+    print( 'CONNECTION CREATED')
+    cur = conn.cursor()
+    print( 'CUR DONE')
+    engine = create_engine(f'postgresql://{postgres_username}:{postgres_password}@localhost:5432/{new_database}')
+    #df_unavailable_sessions = create_dataframe_from_excel_sheet('unavailable_sessions_.xlsx', 'Unavailable sessions')
+    #print( df_unavailable_sessions.head(20))
+    #df_unavailable_sessions_part = df_unavailable_sessions.iloc[:200]
+    create_database( cur, conn, new_database)
+    engine = create_engine(f'postgresql://{postgres_username}:{postgres_password}@localhost:5432/{new_database}')
+    file_excel = None
+    if file_to_read.endswith('.xlsx'):
+        file_excel = file_to_read
+    if file_excel != None:
+        excel_file = pd.ExcelFile( file_excel)
+        sheets_names = excel_file.sheet_names
+        sheets_names = sheets_names[2:]
+        #create_database( cur, conn, new_database)
+        for sheet in sheets_names:
+            df = create_dataframe_from_excel_sheet( file_to_read, sheet)
+            print( f'sheet - > {sheet}')
+            print( df.head(15) ) 
+            data_frame_to_sql( df, sheet, engine)
+            # read_tabel( cur, conn, table_name)
+    conn.commit()
+    conn.close()
